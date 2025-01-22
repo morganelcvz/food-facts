6684e955d7e6770ba9aac1f487fa0d551d7e587e
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2025-01-22 16:06:52 +0100

    ajout exercice V1 complet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,75 @@
-fetch('https://world.openfoodfacts.org/api/v3/product/7613035676497')
+const url = new URLSearchParams(window.location.search);
+let code = url.get('code')
+
+if (code == null || code == "") {
+    code = '7613032655495'
+} 
+
+
+fetch(`https://world.openfoodfacts.org/api/v3/product/${code}`)
 .then(response => response.json())
 .then(data => {
     console.log(data.product)
+    console.log(data.code)
     console.log(data.product.abbreviated_product_name)
-    console.log(data.product._id)
+    console.log(data.product.product_name_fr_imported)
+    console.log(data.product.generic_name)
+    console.log(data.product.brands)
+    console.log(data.product.product_quantity)
+    console.log(data.product.categories)
+    console.log(data.product.nutriscore_2023_tags)
+    console.log(data.product.nova_group)
+    console.log(data.product.selected_images.front.display.fr)
+    console.log(data.product.countries_imported)
+
+    document.getElementById("produit").innerHTML = `
+                <div class="p-box">
+                <div class="p-left">
+                <img src="${data.product.selected_images.front.display.fr}">
+                <div class="p-code">code barre : ${data.code}</div>
+                </div>
+                <div class="box-info">
+                    <h1>${data.product.product_name_fr_imported == undefined ? `${data.product.product_name},  ${data.product.quantity}` : `${data.product.product_name_fr_imported}`}</h1>
+                    <span>${data.product.generic_name == undefined ? `${data.product.product_name}` : `${data.product.generic_name}`}</span>
+                    <div class="sub-info">
+                        <span>
+                            <b>Quantité</b>
+                            <br/>
+                            ${data.product.product_quantity}g 
+                        </span>
+                        <span>
+                            <b>Marques</b>
+                            <br/>
+                            ${data.product.brands} 
+                        </span>
+                        <span>
+                            <b>Pays</b>
+                            <br/>
+                            ${data.product.countries_imported}
+                        </span>
+                    </div>
+                    <div class="score">
+                    <img src="assets/img/nutriscore-${data.product.nutriscore_2023_tags}-new-en.svg">
+                    <img src="assets/img/nova-group-${data.product.nova_group}.svg">
+                    </div>
+                    <div class="tags">
+                    ${createCategories(data.product.categories)}
+                    </div>
+                </div>
+            </div>
+    `
     
 })
 .catch(error => console.error(error))
 
-// 1. nom du produit 2. code barre 3. dénomination générique 4. quantité 5. marque 6. catégorie 7. photo 
-// 8. nutriscore (grade) 9. indice nova 
+function createCategories(cat) {
+    let myDiv = ""
+    let test = cat.split(", ") 
+    test.forEach(element => {
+        myDiv += `
+        <span>${element}</span>
+        `
+    }); 
+
+    return myDiv 
+}
